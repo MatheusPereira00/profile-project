0ecffacec25015e4cb83f62a437a30089b7f8ba5
Lucas Peixoto <lspeixoto@stefanini.com>
2022-05-13 10:15:37 -0300

    Inclusão de download do cv

--- src/app/profile/topics/personal/personal.component.ts
+++ src/app/profile/topics/personal/personal.component.ts
@@ -3,10 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
 	selector: 'app-personal',
 	templateUrl: './personal.component.html',
-	styles: [''],
+	styles: [``],
 })
 export class PersonalComponent implements OnInit {
 	constructor() {}
 
 	ngOnInit() {}
+
+	openCurriculum() {
+		const curriculumLink =
+			'https://drive.google.com/file/d/1jZpsi2Fx-aAJLzHLrSTh4An3BymtM9F_/view?usp=sharing';
+		window.open(curriculumLink, '_blank');
+	}
 }
--- src/app/shared/modules/shared-common.module.ts
+++ src/app/shared/modules/shared-common.module.ts
@@ -1,26 +1,28 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-import { TranslocoRootModule } from '@shared/modules/transloco-root.module';
-import { PrimeModule } from '@shared/modules/prime.module';
-import { HeaderComponent } from '@shared/components/header/header.component';
 import { FooterComponent } from '@shared/components/footer/footer.component';
+import { HeaderComponent } from '@shared/components/header/header.component';
+import { PrimeModule } from '@shared/modules/prime.module';
+import { TranslocoRootModule } from '@shared/modules/transloco-root.module';
 
-import { LoadingComponent } from '@shared/components/loading/loading.component';
-import { ErrorMessageComponent } from '@shared/components/error-message/error-message.component';
 import { AboutMeComponent } from '@app/profile/topics/about-me/about-me.component';
+import { CertificationsComponent } from '@app/profile/topics/certifications/certifications.component';
 import { ExperiencesComponent } from '@app/profile/topics/experiences/experiences.component';
 import { FormationsComponent } from '@app/profile/topics/formations/formations.component';
+import { PersonalComponent } from '@app/profile/topics/personal/personal.component';
 import { SkillsComponent } from '@app/profile/topics/skills/skills.component';
-import { CertificationsComponent } from '@app/profile/topics/certifications/certifications.component';
+import { ErrorMessageComponent } from '@shared/components/error-message/error-message.component';
+import { LoadingComponent } from '@shared/components/loading/loading.component';
 
 @NgModule({
 	declarations: [
 		HeaderComponent,
 		FooterComponent,
 		LoadingComponent,
+    PersonalComponent,
 		AboutMeComponent,
 		ExperiencesComponent,
 		CertificationsComponent,
@@ -41,6 +43,7 @@ import { CertificationsComponent } from '@app/profile/topics/certifications/cert
 		HeaderComponent,
 		FooterComponent,
 		LoadingComponent,
+    PersonalComponent,
 		AboutMeComponent,
 		ExperiencesComponent,
 		CertificationsComponent,
